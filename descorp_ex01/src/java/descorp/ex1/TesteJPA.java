b94cfe2d6316f0cd6b0d58880ace5d16b6623c69
Michel Nascimento <michel_henrique_911@hotmail.com>
2018-03-16 19:16:27 -0300

    exemplo01 descorp

---
+++ descorp_ex01/src/java/descorp/ex1/TesteJPA.java
@@ -0,0 +1,41 @@
+package descorp.ex1;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class TesteJPA {
+
+    public static void main(String[] args) {
+        Usuario usuario = new Usuario();
+        preencherUsuario(usuario);
+        EntityManagerFactory emf = null;
+        EntityManager em = null;
+        EntityTransaction et = null;
+        try {
+            //EntityManagerFactory realiza a leitura das informações relativas à "persistence-unit".
+            emf = Persistence.createEntityManagerFactory("exemplo_01");
+            em = emf.createEntityManager(); //Criação do EntityManager.
+            et = em.getTransaction(); //Recupera objeto responsável pelo gerenciamento de transação.
+            et.begin();
+            em.persist(usuario);
+            et.commit();
+        } catch (Exception ex) {
+            if (et != null)
+                et.rollback();
+        } finally {
+            if (em != null)
+                em.close();       
+            if (emf != null)
+                emf.close();
+        }
+    }
+
+    private static void preencherUsuario(Usuario usuario) {
+        usuario.setNome("Teste Web Sem o Maven");
+        usuario.setEmail("email@gmail.com");
+        usuario.setLogin("sem maven");
+        usuario.setSenha("senha");
+    }
+}
